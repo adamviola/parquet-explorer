917d601c8fa2c1c29dce1340a42d3c932dbd5fdc
Adam Viola <adam@viola.dev>
2026-02-13 23:36:17 -0500

    Add better formatting for blobs

--- src/parquetDocument.ts
+++ src/parquetDocument.ts
@@ -4,13 +4,14 @@ import { getNonce } from './util';
 import { parse } from "path"
 
 import * as duckdb from 'duckdb';
+import { validateHeaderName } from 'http';
 
 interface IMessage {
-    type: 'query' | 'more';
-    success: boolean;
-    message?: string;
-    results?: duckdb.TableData;
-    describe?: duckdb.TableData;
+	type: 'query' | 'more';
+	success: boolean;
+	message?: string;
+	results?: duckdb.TableData;
+	describe?: duckdb.TableData;
 }
 
 /**
@@ -18,211 +19,213 @@ interface IMessage {
  */
 class ParquetDocument extends Disposable implements vscode.CustomDocument {
 
-    static async create(
-        uri: vscode.Uri,
-        backupId: string | undefined,
-    ): Promise<ParquetDocument | PromiseLike<ParquetDocument>> {
-        // If we have a backup, read that. Otherwise read the resource from the workspace
-        const trueURI = typeof backupId === 'string' ? vscode.Uri.parse(backupId) : uri;
-        return new ParquetDocument(trueURI);
-    }
+	static async create(
+		uri: vscode.Uri,
+		backupId: string | undefined,
+	): Promise<ParquetDocument | PromiseLike<ParquetDocument>> {
+		// If we have a backup, read that. Otherwise read the resource from the workspace
+		const trueURI = typeof backupId === 'string' ? vscode.Uri.parse(backupId) : uri;
+		return new ParquetDocument(trueURI);
+	}
 
-    private readonly _uri: vscode.Uri;
-    private readonly _db: duckdb.Database;
+	private readonly _uri: vscode.Uri;
+	private readonly _db: duckdb.Database;
 
-    private constructor(uri: vscode.Uri) {
-        super();
-        this._uri = uri;
-        this._db = new duckdb.Database(':memory:');
+	private constructor(uri: vscode.Uri) {
+		super();
+		this._uri = uri;
+		this._db = new duckdb.Database(':memory:');
 
-        const config = vscode.workspace.getConfiguration('parquet-explorer')
-        let tableName: string = config.get("tableName")!;
-        if (config.get("useFileNameAsTableName"))
-            tableName = parse(uri.fsPath).name
+		const config = vscode.workspace.getConfiguration('parquet-explorer')
+		let tableName: string = config.get("tableName")!;
+		if (config.get("useFileNameAsTableName"))
+			tableName = parse(uri.fsPath).name
 
-        this.db.exec(
-            `CREATE VIEW ${tableName} AS SELECT * FROM read_parquet('${uri.fsPath}');`
-        );
-    }
+		this.db.exec(
+			`CREATE VIEW ${tableName} AS SELECT * FROM read_parquet('${uri.fsPath}');`
+		);
+	}
 
-    public get uri() { return this._uri; }
-    public get db() { return this._db; }
+	public get uri() { return this._uri; }
+	public get db() { return this._db; }
 
-    private readonly _onDidDispose = this._register(new vscode.EventEmitter<void>());
-    /**
-     * Fired when the document is disposed of.
-     */
-    public readonly onDidDispose = this._onDidDispose.event;
+	private readonly _onDidDispose = this._register(new vscode.EventEmitter<void>());
+	/**
+	 * Fired when the document is disposed of.
+	 */
+	public readonly onDidDispose = this._onDidDispose.event;
 
-    /**
-     * Called by VS Code when there are no more references to the document.
-     *
-     * This happens when all editors for it have been closed.
-     */
-    dispose(): void {
-        this._onDidDispose.fire();
-        super.dispose();
-    }
+	/**
+	 * Called by VS Code when there are no more references to the document.
+	 *
+	 * This happens when all editors for it have been closed.
+	 */
+	dispose(): void {
+		this._onDidDispose.fire();
+		super.dispose();
+	}
 
-    private formatSql(sql: string, limit: number, offset: number): string {
-        return `SELECT * FROM (\n${sql.replace(';', '')}\n) LIMIT ${limit} OFFSET ${offset}`;
-    }
+	private formatSql(sql: string, limit: number, offset: number): string {
+		return `SELECT * FROM (\n${sql.replace(';', '')}\n) LIMIT ${limit} OFFSET ${offset}`;
+	}
 
-    private cleanResults(results: duckdb.TableData): duckdb.TableData {
+	private cleanResults(results: duckdb.TableData): duckdb.TableData {
 		const formatter = (key: string, value: any) => {
 			if (typeof value == "bigint")
 				return value.toString()
+			if (Buffer.isBuffer(value))
+				return `0x${value.toString('hex').toUpperCase()}`
 			return value;
 		}
 
-        for (const row of results) {
-            for (const [key, value] of Object.entries(row)) {
-				if (typeof value == "object" && Object.keys(value).length > 0 && !Array.isArray(value))
+		for (const row of results) {
+			for (const [key, value] of Object.entries(row)) {
+				if (typeof value == "object" && Object.keys(value).length > 0 && !Array.isArray(value) && !Buffer.isBuffer(value))
 					row[key] = JSON.stringify(value, formatter, 2);
 				else if (Array.isArray(value))
 					row[key] = JSON.stringify(value, formatter)
 				else
 					row[key] = formatter(key, value);
-            }
-        }
-        return results;
-    }
+			}
+		}
+		return results;
+	}
 
-    runQuery(sql: string, limit: number, callback: (msg: IMessage) => void): void {
-        // Fetch resulting column names and types
-        this.db.all(
-            `DESCRIBE (${sql.replace(';', '')});`,
-            (err, descRes) => {
-                if (err) {
-                    callback({ type: 'query', success: false, message: err.message });
-                    return;
-                }
+	runQuery(sql: string, limit: number, callback: (msg: IMessage) => void): void {
+		// Fetch resulting column names and types
+		this.db.all(
+			`DESCRIBE (${sql.replace(';', '')});`,
+			(err, descRes) => {
+				if (err) {
+					callback({ type: 'query', success: false, message: err.message });
+					return;
+				}
 
-                // Execute query
-                this.db.all(
-                    this.formatSql(sql, limit, 0),
-                    (err, res) => {
-                        if (err) {
-                            callback({ type: 'query', success: false, message: err.message });
-                            return;
-                        }
-                        callback({ type: 'query', success: true, results: this.cleanResults(res), describe: descRes });
-                    }
-                );
-            }
-        );
-    }
+				// Execute query
+				this.db.all(
+					this.formatSql(sql, limit, 0),
+					(err, res) => {
+						if (err) {
+							callback({ type: 'query', success: false, message: err.message });
+							return;
+						}
+						callback({ type: 'query', success: true, results: this.cleanResults(res), describe: descRes });
+					}
+				);
+			}
+		);
+	}
 
-    fetchMore(sql: string, limit: number, offset: number, callback: (msg: IMessage) => void): void {
-        this.db.all(
-            this.formatSql(sql, limit, offset),
-            (err, res) => {
-                if (err) {
-                    callback({ type: 'more', success: false, message: err.message });
-                    return;
-                }
-                callback({ type: 'more', success: true, results: this.cleanResults(res) });
-            }
-        );
-    }
+	fetchMore(sql: string, limit: number, offset: number, callback: (msg: IMessage) => void): void {
+		this.db.all(
+			this.formatSql(sql, limit, offset),
+			(err, res) => {
+				if (err) {
+					callback({ type: 'more', success: false, message: err.message });
+					return;
+				}
+				callback({ type: 'more', success: true, results: this.cleanResults(res) });
+			}
+		);
+	}
 }
 
 
 export class ParquetDocumentProvider implements vscode.CustomReadonlyEditorProvider<ParquetDocument> {
 
-    public static register(context: vscode.ExtensionContext): vscode.Disposable {
-        return vscode.window.registerCustomEditorProvider(
-            ParquetDocumentProvider.viewType,
-            new ParquetDocumentProvider(context),
-            { supportsMultipleEditorsPerDocument: false },
-        );
-    }
+	public static register(context: vscode.ExtensionContext): vscode.Disposable {
+		return vscode.window.registerCustomEditorProvider(
+			ParquetDocumentProvider.viewType,
+			new ParquetDocumentProvider(context),
+			{ supportsMultipleEditorsPerDocument: false },
+		);
+	}
 
-    private static readonly viewType = 'parquetExplorer.explorer';
+	private static readonly viewType = 'parquetExplorer.explorer';
 
-    constructor(
-        private readonly _context: vscode.ExtensionContext
-    ) { }
+	constructor(
+		private readonly _context: vscode.ExtensionContext
+	) { }
 
-    async openCustomDocument(
-        uri: vscode.Uri,
-        openContext: { backupId?: string },
-        _token: vscode.CancellationToken
-    ): Promise<ParquetDocument> {
-        const document: ParquetDocument = await ParquetDocument.create(uri, openContext.backupId);
-        return document;
-    }
+	async openCustomDocument(
+		uri: vscode.Uri,
+		openContext: { backupId?: string },
+		_token: vscode.CancellationToken
+	): Promise<ParquetDocument> {
+		const document: ParquetDocument = await ParquetDocument.create(uri, openContext.backupId);
+		return document;
+	}
 
-    async resolveCustomEditor(
-        document: ParquetDocument,
-        webviewPanel: vscode.WebviewPanel,
-        _token: vscode.CancellationToken
-    ): Promise<void> {
-        // Setup initial content for the webview
-        webviewPanel.webview.options = {
-            enableScripts: true,
-        };
+	async resolveCustomEditor(
+		document: ParquetDocument,
+		webviewPanel: vscode.WebviewPanel,
+		_token: vscode.CancellationToken
+	): Promise<void> {
+		// Setup initial content for the webview
+		webviewPanel.webview.options = {
+			enableScripts: true,
+		};
 
-        webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview, document.uri);
+		webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview, document.uri);
 
-        webviewPanel.webview.onDidReceiveMessage(e => this.onMessage(document, webviewPanel, e));
-    }
-
-
-    /**
-     * Get the static HTML used for in our editor's webviews.
-     */
-    private getHtmlForWebview(webview: vscode.Webview, uri: vscode.Uri): string {
-        // Local path to script and css for the webview
-        const jsUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'parquetExplorer.js'));
-
-        const cssUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'parquetExplorer.css'));
-
-        const codeInputJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'code-input.min.js'));
-
-        const codeInputCssUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'code-input.min.css'));
-
-        const indentJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'indent.min.js'));
-
-        const prismJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'prism.js'));
-
-        const prismCssUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'prism.css'));
-        
-        const luxonJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'luxon.min.js'));
-
-        const tabulatorJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'tabulator.min.js'));
-
-        const tabulatorCssUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'tabulator.min.css'));
-
-        const loadingIconUri = webview.asWebviewUri(vscode.Uri.joinPath(
-            this._context.extensionUri, 'media', 'loading_icon.gif'));
+		webviewPanel.webview.onDidReceiveMessage(e => this.onMessage(document, webviewPanel, e));
+	}
 
 
-        // Use a nonce to whitelist which scripts can be run
-        const nonce = getNonce();
+	/**
+	 * Get the static HTML used for in our editor's webviews.
+	 */
+	private getHtmlForWebview(webview: vscode.Webview, uri: vscode.Uri): string {
+		// Local path to script and css for the webview
+		const jsUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'parquetExplorer.js'));
 
-        const config = vscode.workspace.getConfiguration('parquet-explorer')
-        let tableName: string = config.get("tableName")!;
-        if (config.get("useFileNameAsTableName"))
-            tableName = parse(uri.fsPath).name
+		const cssUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'parquetExplorer.css'));
 
-        let defaultQuery: string = config.get("defaultQuery")!;
-        defaultQuery = defaultQuery.replace(/\${[^{]+}/g, (match) => {
-            const name = match.slice(2, -1).trim();
-            return name == "tableName" ? tableName : "";
-        });
+		const codeInputJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'code-input.min.js'));
 
-        return `
+		const codeInputCssUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'code-input.min.css'));
+
+		const indentJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'indent.min.js'));
+
+		const prismJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'prism.js'));
+
+		const prismCssUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'prism.css'));
+
+		const luxonJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'luxon.min.js'));
+
+		const tabulatorJsUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'tabulator.min.js'));
+
+		const tabulatorCssUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'tabulator.min.css'));
+
+		const loadingIconUri = webview.asWebviewUri(vscode.Uri.joinPath(
+			this._context.extensionUri, 'media', 'loading_icon.gif'));
+
+
+		// Use a nonce to whitelist which scripts can be run
+		const nonce = getNonce();
+
+		const config = vscode.workspace.getConfiguration('parquet-explorer')
+		let tableName: string = config.get("tableName")!;
+		if (config.get("useFileNameAsTableName"))
+			tableName = parse(uri.fsPath).name
+
+		let defaultQuery: string = config.get("defaultQuery")!;
+		defaultQuery = defaultQuery.replace(/\${[^{]+}/g, (match) => {
+			const name = match.slice(2, -1).trim();
+			return name == "tableName" ? tableName : "";
+		});
+
+		return `
             <!DOCTYPE html>
             <html lang="en">
             <head>
@@ -275,22 +278,22 @@ export class ParquetDocumentProvider implements vscode.CustomReadonlyEditorProvi
             
             </html>
         `;
-    }
+	}
 
 
-    private postMessage(panel: vscode.WebviewPanel, message: IMessage): void {
-        panel.webview.postMessage(message);
-    }
+	private postMessage(panel: vscode.WebviewPanel, message: IMessage): void {
+		panel.webview.postMessage(message);
+	}
 
-    private onMessage(document: ParquetDocument, panel: vscode.WebviewPanel, message: any) {
-        switch (message.type) {
-            case 'query':
-                document.runQuery(message.sql, message.limit, (msg: IMessage) => this.postMessage(panel, msg));
-                return;
-            case 'more':
-                document.fetchMore(message.sql, message.limit, message.offset, (msg: IMessage) => this.postMessage(panel, msg));
-                return;
+	private onMessage(document: ParquetDocument, panel: vscode.WebviewPanel, message: any) {
+		switch (message.type) {
+			case 'query':
+				document.runQuery(message.sql, message.limit, (msg: IMessage) => this.postMessage(panel, msg));
+				return;
+			case 'more':
+				document.fetchMore(message.sql, message.limit, message.offset, (msg: IMessage) => this.postMessage(panel, msg));
+				return;
 
-        }
-    }
+		}
+	}
 }
